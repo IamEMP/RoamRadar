fefc447a0de3983fa1bc885193b13974e769d8b6
Ethan Phillips <ethansemail@yahoo.com>
2023-10-01 12:56:53 -0500

    Initial Commit

---
+++ RoamRadar/RoamRadarApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoamRadarApp.swift
+//  RoamRadar
+//
+//  Created by Ethan Phillips on 10/1/23.
+//
+
+import SwiftUI
+
+@main
+struct RoamRadarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
