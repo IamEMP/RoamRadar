ac05fcc2d4f1506a9d373559d707e09df2b2cb5c
Ethan Phillips <ethansemail@yahoo.com>
2023-10-03 15:50:09 -0500

    Roam Radar is a basic example of the use of Swift Data and how to store data with the new Apple framwork. Will be using this project as a test for Swift Data.

--- RoamRadar/ContentView.swift
+++ RoamRadar/ContentView.swift
@@ -6,16 +6,38 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct ContentView: View {
+    @Environment(\.modelContext) var modelContext
+    @Query var destinations: [Destination]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List {
+                ForEach(destinations) { destination in
+                    VStack(alignment: .leading)  {
+                        Text(destination.name)
+                            .font(.headline)
+                        
+                        Text(destination.date.formatted(date: .long, time: .shortened))
+                    }
+                }
+            }
+            .navigationTitle("Roam Radar")
+            .toolbar {
+                Button("Add Samples", action: addSamples)
+            }
         }
-        .padding()
+    }
+    func addSamples() {
+        let rome = Destination(name: "Rome")
+        let canada = Destination(name: "Canada")
+        let grandCanyon = Destination(name: "Grand Canyon")
+        
+        modelContext.insert(rome)
+        modelContext.insert(canada)
+        modelContext.insert(grandCanyon)
     }
 }
 
---
+++ RoamRadar/Destination.swift
@@ -0,0 +1,24 @@
+//
+//  Destination.swift
+//  RoamRadar
+//
+//  Created by Ethan Phillips on 10/1/23.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+class Destination {
+    var name: String
+    var details: String
+    var date: Date
+    var priority: Int
+    
+    init(name: String = "", details: String = "", date: Date = .now, priority: Int = 2) {
+        self.name = name
+        self.details = details
+        self.date = date
+        self.priority = priority
+    }
+}
