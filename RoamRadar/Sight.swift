6711026f93b402c6f16742a7291cc62928b987c7
Ethan Phillips <ethansemail@yahoo.com>
2023-10-07 11:26:58 -0500

    Added destinations - Added TextField and button that lets you add destinations to the trip.

---
+++ RoamRadar/Sight.swift
@@ -0,0 +1,18 @@
+//
+//  Sight.swift
+//  RoamRadar
+//
+//  Created by Ethan Phillips on 10/7/23.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+class Sight {
+    var name: String
+    
+    init(name: String) {
+        self.name = name
+    }
+}
